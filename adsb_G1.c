ba3c83b76f0f83db463eff2fd4d2aa52d526fadb
Dai Fukunaga <dai.pochi.700@gmail.com>
2023-12-24 01:48:19 +0900

    #6 finished to implement prefix sum

--- adsb_G1.c
+++ adsb_G1.c
@@ -1,19 +1,79 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int n, m, p, k;
 int *l, *s, *A;
 int **X;
 
+int model = 0;
+
 void input_data(int, char **);
 
 int main(int argc, char **argv) {
+    clock_t tic, toc;
+    struct timespec start, end;
+    tic = clock();
+    clock_gettime(CLOCK_REALTIME, &start);
     input_data(argc, argv);
+
+    if (model == 1) {
+        int **ans = (int **)malloc(sizeof(int *) * m);
+// #pragma omp parallel for // 並列処理を試したい場合は、この行のコメントアウトを外してください
+        for (int i = 0; i < m; i++) {
+            int ceil = (8 < m - i) ? 8 : m - i;
+            ans[i] = (int *)calloc(sizeof(int), k);
+            for (int j = 0; j < n; j++) {
+                for (int l = 0; l < ceil; l++) {
+                    if (A[i + l] == X[j][i + l + 1]) {
+                        ans[i][l]++;
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
+        FILE *file = fopen(argv[3], "w");
+        for (int i = 0; i < p; i++) {
+            fprintf(file, "%d, %d, %d\n", l[i], s[i], ans[l[i]][s[i] - 1]);
+        }
+        fclose(file);
+    } else if (model == 2) {
+// #pragma omp parallel for // 並列処理を試したい場合は、この行のコメントアウトを外してください
+        for (int i = 0; i < n; i++) {
+            X[i][1] = (X[i][1] == A[0]) ? 1 : 0;
+            for (int j = 2; j <= m; j++) {
+                if (X[i][j] == A[j - 1]) {
+                    X[i][j] = X[i][j - 1] + 1;
+                } else {
+                    X[i][j] = X[i][j - 1];
+                }
+            }
+        }
+        FILE *file = fopen(argv[3], "w");
+        for (int i = 0; i < p; i++) {
+            int ans = 0;
+            for (int j = 0; j < n; j++) {
+                if (s[i] == (X[j][l[i] + s[i]] - X[j][l[i]])) {
+                    ans++;
+                }
+            }
+            fprintf(file, "%d, %d, %d\n", l[i], s[i], ans);
+        }
+        fclose(file);
+    }
 
     free(l);
     free(s);
     free(X);
     free(A);
+
+    toc = clock();
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("clock: %lf\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+    printf("time: %lf\n",
+           ((double)end.tv_sec - (double)start.tv_sec) +
+               ((double)end.tv_nsec - (double)start.tv_nsec) / 1e9);
     return 0;
 }
 
@@ -33,8 +93,10 @@ void input_data(int argc, char **argv) {
     if (fgets(line, sizeof(line), file) != NULL) {
         if (line[5] == '1') {
             n = 1000, m = 10000, p = 30000, k = 8;
+            model = 1;
         } else {
             n = 1000, m = 10000, p = 50000, k = 300;
+            model = 2;
         }
     }
     X = (int **)malloc(sizeof(int *) * n);
@@ -62,9 +124,10 @@ void input_data(int argc, char **argv) {
     }
 
     for (int i = 0; fgets(line, sizeof(line), file) != NULL; i++) {
-        X[i] = (int *)malloc(sizeof(int) * m);
+        X[i] = (int *)malloc(sizeof(int) * (m + 1));
+        X[i][0] = 0;
         for (int j = 0; j < m; j++) {
-            X[i][j] = line[j] - '0';
+            X[i][j + 1] = line[j] - '0';
         }
     }
 }
