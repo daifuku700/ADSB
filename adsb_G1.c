77b037cdf04aaa36fef055fbac0925aa6aeecae4
Dai Fukunaga <dai.pochi.700@gmail.com>
2024-01-12 15:26:41 +0900

    save

--- adsb_G1.c
+++ adsb_G1.c
@@ -13,7 +13,7 @@ int n, m, p, k;
 
 int model = 0;
 
-int thread_num = 4;
+int thread_num = 1;
 
 char line[(int)1e4 + 10];
 char A[10000 + 10];
@@ -49,12 +49,12 @@ int main(int argc, char **argv) {
     tic = clock();
     clock_gettime(CLOCK_REALTIME, &start);
 
-    FILE *range = fopen(argv[2], "r");
-    if (range == NULL) {
+    FILE *rg = fopen(argv[2], "r");
+    if (rg == NULL) {
         perror("could not open the file\n");
         exit(2);
     }
-    if (fgets(line, sizeof(line), range) != NULL) {
+    if (fgets(line, sizeof(line), rg) != NULL) {
         if (line[5] == '1') {
             n = 1000, m = 10000, p = 30000, k = 8;
             model = 1;
@@ -86,27 +86,37 @@ int main(int argc, char **argv) {
         for (int i = 0; i < m; i++) {
             ans[i] = (us *)calloc(sizeof(us), 8);
         }
-        pthread_t threads[thread_num];
-        start_end se[thread_num];
-        int chunk_size = m / thread_num;
-        for (int i = 0; i < thread_num; i++) {
-            se[i].start = i * chunk_size;
-            se[i].end = (i == thread_num - 1) ? m : se[i].start + chunk_size;
-        }
-        for (; fgets(line, sizeof(line), data) != NULL;) {
-            for (int i = 0; i < thread_num; i++) {
-                pthread_create(&threads[i], NULL, model1, &se[i]);
-            }
+        // pthread_t threads[32];
+        // start_end se[32];
+        // int chunk_size = m / thread_num;
+        // for (int i = 0; i < thread_num; i++) {
+        //     se[i].start = i * chunk_size;
+        //     se[i].end = (i == thread_num - 1) ? m : se[i].start + chunk_size;
+        // }
+        // for (; fgets(line, sizeof(line), data) != NULL;) {
+        //     for (int i = 0; i < thread_num; i++) {
+        //         pthread_create(&threads[i], NULL, model1, &se[i]);
+        //     }
 
-            for (int i = 0; i < thread_num; i++) {
-                pthread_join(threads[i], NULL);
+        //     for (int i = 0; i < thread_num; i++) {
+        //         pthread_join(threads[i], NULL);
+        //     }
+        // }
+        for (; fgets(line, sizeof(line), data) != NULL;) {
+            for (int i = 0; i < m; i++) {
+                for (int h = 0; h < 8; h++) {
+                    if (A[i + h] == line[i + h]) {
+                        ans[i][h]++;
+                    } else {
+                        break;
+                    }
+                }
             }
         }
         int l, s;
-        for (; fscanf(range, "%d, %d/n", &l, &s) != EOF;) {
+        for (; fscanf(rg, "%d, %d/n", &l, &s) != EOF;) {
             fprintf(output, "%d, %d, %d\n", l, s, ans[l][s - 1]);
         }
-        printf("%d, %d, %d\n", l, s, ans[l][s - 1]);
     } else if (model == 2) {
         return 0;
         // for (int i = 0; i < p; i++) {
@@ -121,7 +131,7 @@ int main(int argc, char **argv) {
     }
 
     fclose(data);
-    fclose(range);
+    fclose(rg);
     fclose(output);
 
     toc = clock();
