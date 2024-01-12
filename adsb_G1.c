4946f61f88fb107912bbde40a63bd2259337905f
Dai Fukunaga <dai.pochi.700@gmail.com>
2024-01-12 13:42:39 +0900

    minimize model 1

--- adsb_G1.c
+++ adsb_G1.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <time.h>
 
+typedef unsigned short us;
+
 int n, m, p, k;
-int *l, *s, *A;
-int **data;
 
 int model = 0;
 
@@ -13,72 +13,24 @@ void input_data(int, char **);
 int main(int argc, char **argv) {
     clock_t tic, toc;
     struct timespec start, end;
-    tic = clock();
-    clock_gettime(CLOCK_REALTIME, &start);
-    input_data(argc, argv);
 
-    if (model == 1) {
-        int **ans = (int **)malloc(sizeof(int *) * m);
-        for (int i = 0; i < m; i++) {
-            int ceil = (8 < m - i) ? 8 : m - i;
-            ans[i] = (int *)calloc(sizeof(int), k);
-            for (int j = 0; j < n; j++) {
-                for (int l = 0; l < ceil; l++) {
-                    if (A[i + l] == data[j][i + l + 1]) {
-                        ans[i][l]++;
-                    } else {
-                        break;
-                    }
-                }
-            }
-        }
-        FILE *file = fopen(argv[3], "w");
-        for (int i = 0; i < p; i++) {
-            fprintf(file, "%d, %d, %d\n", l[i], s[i], ans[l[i]][s[i] - 1]);
-        }
-        fclose(file);
-    } else if (model == 2) {
-        FILE *file = fopen(argv[3], "w");
-        for (int i = 0; i < p; i++) {
-            int ans = 0;
-            for (int j = 0; j < n; j++) {
-                if (s[i] == (data[j][l[i] + s[i]] - data[j][l[i]])) {
-                    ans++;
-                }
-            }
-            fprintf(file, "%d, %d, %d\n", l[i], s[i], ans);
-        }
-        fclose(file);
-    }
-
-    free(l);
-    free(s);
-    free(data);
-    free(A);
-
-    toc = clock();
-    clock_gettime(CLOCK_REALTIME, &end);
-    printf("clock: %lf\n", (double)(toc - tic) / CLOCKS_PER_SEC);
-    printf("time: %lf\n",
-           ((double)end.tv_sec - (double)start.tv_sec) +
-               ((double)end.tv_nsec - (double)start.tv_nsec) / 1e9);
-    return 0;
-}
-
-void input_data(int argc, char **argv) {
     if (argc != 4) {
         perror("the number of command line arguments should be three\n");
         exit(1);
     }
 
-    FILE *file = fopen(argv[2], "r");
-    if (file == NULL) {
+    tic = clock();
+    clock_gettime(CLOCK_REALTIME, &start);
+
+    char line[(int)1e4 + 10];
+    char A[10000 + 10];
+
+    FILE *range = fopen(argv[2], "r");
+    if (range == NULL) {
         perror("could not open the file\n");
         exit(2);
     }
-
-    char line[(int)1e4 + 2];
-    if (fgets(line, sizeof(line), file) != NULL) {
+    if (fgets(line, sizeof(line), range) != NULL) {
         if (line[5] == '1') {
             n = 1000, m = 10000, p = 30000, k = 8;
             model = 1;
@@ -87,51 +39,67 @@ void input_data(int argc, char **argv) {
             model = 2;
         }
     }
-    data = (int **)malloc(sizeof(int *) * n);
-    l = (int *)malloc(sizeof(int) * p);
-    s = (int *)malloc(sizeof(int) * p);
-    A = (int *)malloc(sizeof(int) * m);
-    int a, b;
-    for (int i = 0; fscanf(file, "%d, %d\n", &a, &b) != EOF; i++) {
-        l[i] = a;
-        s[i] = b;
-    }
 
-    fclose(file);
-
-    file = fopen(argv[1], "r");
-    if (file == NULL) {
+    FILE *data = fopen(argv[1], "r");
+    if (data == NULL) {
         perror("could not open the file\n");
         exit(3);
     }
-
-    if (fgets(line, sizeof(line), file) != NULL) {
+    if (fgets(line, sizeof(line), data) != NULL) {
         for (int j = 0; j < m; j++) {
-            A[j] = line[j] - '0';
+            A[j] = line[j];
         }
+    }
+
+    FILE *output = fopen(argv[3], "w");
+    if (output == NULL) {
+        perror("could not open the file\n");
+        exit(4);
     }
 
     if (model == 1) {
-        for (int i = 0; fgets(line, sizeof(line), file) != NULL; i++) {
-            data[i] = (int *)malloc(sizeof(int) * (m + 1));
-            data[i][0] = 0;
-            for (int j = 0; j < m; j++) {
-                data[i][j + 1] = line[j] - '0';
-            }
+        us **ans = (us **)malloc(sizeof(us *) * m);
+        for (int i = 0; i < m; i++) {
+            ans[i] = (us *)calloc(sizeof(us), 8);
         }
-    } else if (model == 2) {
-        for (int i = 0; fgets(line, sizeof(line), file) != NULL; i++) {
-            data[i] = (int *)malloc(sizeof(int) * (m + 1));
-            data[i][0] = 0;
-            int now = 0, next;
-            for (int j = 0; j < m; j++) {
-                now = next;
-                if (line[j] - '0' == A[j]) {
-                    next = (data[i][j + 1] = now + 1);
-                } else {
-                    next = (data[i][j + 1] = now);
+        for (; fgets(line, sizeof(line), data) != NULL;) {
+            for (int i = 0; i < m; i++) {
+                for (int h = 0; h < 8; h++) {
+                    if (A[i + h] == line[i + h]) {
+                        ans[i][h]++;
+                    } else {
+                        break;
+                    }
                 }
             }
         }
+        int l, s;
+        for (; fscanf(range, "%d, %d/n", &l, &s) != EOF;) {
+            fprintf(output, "%d, %d, %d\n", l, s, ans[l][s - 1]);
+        }
+        printf("%d, %d, %d\n", l, s, ans[l][s - 1]);
+    } else if (model == 2) {
+        return 0;
+        // for (int i = 0; i < p; i++) {
+        //     int ans = 0;
+        //     for (int j = 0; j < n; j++) {
+        //         if (s[i] == (data[j][l[i] + s[i]] - data[j][l[i]])) {
+        //             ans++;
+        //         }
+        //     }
+        //     fprintf(output, "%d, %d, %d\n", l[i], s[i], ans);
+        // }
     }
+
+    fclose(data);
+    fclose(range);
+    fclose(output);
+
+    toc = clock();
+    clock_gettime(CLOCK_REALTIME, &end);
+    printf("clock: %lf\n", (double)(toc - tic) / CLOCKS_PER_SEC);
+    printf("time: %lf\n",
+           ((double)end.tv_sec - (double)start.tv_sec) +
+               ((double)end.tv_nsec - (double)start.tv_nsec) / 1e9);
+    return 0;
 }
